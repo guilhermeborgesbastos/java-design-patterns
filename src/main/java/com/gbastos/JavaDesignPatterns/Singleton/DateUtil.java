2a207566aea3f4ec04622944fde0dd7df37befb9
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-02-01 11:00:49 -0300

    Singleton Pattern - Multi-Threading
    - Turned the Singleton Class synchronized (Thread Safe) in order to
    avoid the creation of multiple instances of a Singleton class;

--- src/main/java/com/gbastos/JavaDesignPatterns/Singleton/DateUtil.java
+++ src/main/java/com/gbastos/JavaDesignPatterns/Singleton/DateUtil.java
@@ -5,15 +5,29 @@ import java.io.Serializable;
 public class DateUtil implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private static DateUtil instante;
+	
+	/*
+	 * Volatile keyword is related with the visibility of variables modified by multiple thread during
+	 * concurrent programming.
+	 * 
+	 * For example:
+	 * Every READ or WRITE of volatile variables will be stored into MAIN Memory and not in CPU cache.
+	 */
+	private static volatile DateUtil instante;
 	
 	private DateUtil() {
 		
 	}
 	
+	/*
+	 * Since requiring a 'lock' it is a very expensive process, the synchronized
+	 * block will be executed just in case of the singleton's instance is NULL.
+	 */
 	public static DateUtil getInstante() {
 		if(instante == null) {
-			instante = new DateUtil();
+			synchronized(DateUtil.class) {
+				instante = new DateUtil();
+			}
 		}
 		return instante;
 	}
